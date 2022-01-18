6bf367e6cddfdf38f34ec0877ae81bf3423a12ac
Joe <jliang7558@gmail.com>
2022-01-18 17:37:44 -0500

    apply search bar css

--- StockDashboard/settings.py
+++ StockDashboard/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core', #controls other apps for this project
+    'main', #controls other apps for this project
     'listItems',
 ]
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'StockDashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'listItems/index.html')
+    return render(request, 'main/base.html')
